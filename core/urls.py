999a22937082ef1708b297c64016957999124fd8
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-11-29 12:17:36 -0300

    utilizando WeasyPrint para transformar pagina html em pdf

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import Index2View, IndexView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('2/', Index2View.as_view(), name='index2'),
 ]
