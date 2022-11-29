939b4443dc3e5bac1d598fc3dec9504dac422204
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-11-29 11:32:16 -0300

    criando arquivo pdf e inserindo dados nele | download do arquivo ou exibir no navegador

--- core/views.py
+++ core/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+import io
 
-# Create your views here.
+from django.http import FileResponse
+from django.views.generic import View
+from reportlab.pdfgen import canvas
+
+
+class IndexView(View):
+    
+    def get(self, request, *args, **kwargs):
+        
+        # Cria um arquivo para receber os dados e gerar o PDF
+        buffer = io.BytesIO() # cria arquivo temporário no disco
+        
+        # Cria o arquivo pdf
+        pdf = canvas.Canvas(buffer)
+        
+        # Insere 'coisas' no PDF
+        pdf.drawString(100, 100, "Geek University")
+        
+        # Após inserir no PDF
+        pdf.showPage()
+        pdf.save()
+        
+        # Para finalizar retornamos o buffer para o início do arquivo
+        buffer.seek(0)
+        
+        # Faz o download direto do arquivo PDF
+        # return FileResponse(buffer, as_attachment=True, filename='relatorio1.pdf')
+        
+        # Abre o PDF direto no navegador
+        return FileResponse(buffer, filename='relatorio1.pdf')
